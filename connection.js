b23a7f925ce1abd380dbc4b595b98d7af029d3b4
Aileen <aileenpalafox2012@gmail.com>
2021-06-24 18:32:41 -0700

    passport local

--- connection.js
+++ connection.js
@@ -9,6 +9,7 @@ async function main(callback) {
     try {
         // Connect to the MongoDB cluster
         await client.connect();
+        console.log("connected")
 
         // Make the appropriate DB calls
         await callback(client);
