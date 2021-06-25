78125c2a13b289a88cf43e2bfb68b57e7077a68e
Aileen <aileenpalafox2012@gmail.com>
2021-06-24 19:14:25 -0700

    deleted uri

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 async function main(callback) {
-    const URI = "mongodb+srv://admin:admin@cluster0.mywe7.mongodb.net/agenda?retryWrites=true&w=majority";//process.env.MONGO_URI; // Declare MONGO_URI in your .env file
+    const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
     const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
     try {
